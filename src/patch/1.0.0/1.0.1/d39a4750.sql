f2222f00fc8be4471c532b008a18f225958eca62
Alexei Yashkov <alexei.yashkov@yashkov.org>
2026-05-15 14:22:19 -0400

    Add migration script 1.0.0 -> 1.0.1

---
+++ src/patch/1.0.0/1.0.1/d39a4750.sql
@@ -0,0 +1,12 @@
+-- $d39a4750:
+--   columns:
+--     $0dca8fb7:
+--       type: varchar(256)
+--     $3e0ee234:
+--       type: varchar(256)
+
+alter table person modify name varchar(256) not null
+/
+
+alter table person modify email varchar(256)
+/
